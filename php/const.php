acdb6d10be17443dd6091c987ca8d122f9d75034
Detroix23 <detroix23@gmail.com>
2025-08-29 10:53:25 +0300

    Started cleaning and declaring strict types.

--- php/const.php
+++ php/const.php
@@ -2,6 +2,8 @@
 	/**
 	*  Variables: Constants that might be used often to reduce writtings or flags
 	*/
+	declare(strict_types=1);
+
 
 	///Formatting
 	define('HTML_EOL', "<br>" . PHP_EOL);
@@ -14,16 +16,13 @@
 	/// Env consts
 
 	try {
-		define('CONN_CREDITS', ["newman", "studio", "85125141.23"]);
+		define('CONN_CREDITS', ["newman", "studio", "1"]);
 	} catch (Exception $e) {
 		$e;
 	}
 	const CONN_CREDITS_HOST = 'newman';
 	const CONN_CREDITS_USER = 'studio';
-	const CONN_CREDITS_PWD = '85125141.23';
-	const DB_PREF = "";
-
-
-
+	const CONN_CREDITS_PWD = '1';
+	const DB_PREF = '';
 
 ?>
