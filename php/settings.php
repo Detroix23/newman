d6e14250b56bbf6feb2aca4c6f0dd5558528eda8
Detroix23 <detroix23@gmail.com>
2025-08-29 13:42:13 +0300

    Settings - Variable names.

--- php/settings.php
+++ php/settings.php
@@ -2,18 +2,36 @@
 <h1>Settings</h1>
 
 <form method="post" action="./session_destroyer.php">
-    <input type="submit" class="btn-ctr" id="btn-sessionDestroyer" class="btn-sessionDestroyer" 
-    name="fGoToSessionDestroyer" value="Destroyer of session">
+    <input 
+      type="submit" 
+      class="btn-ctr" 
+      name="fGoToSessionDestroyer" 
+      value="Destroyer of session"
+    />
 </form>
 
 <label for="form-debug">Debug mode</label>
 <form method="post" id="form-debug">
     <label for="btn-check-debug-on" class="btn-ctr">On: </label>
-    <input type="radio" id="btn-check-debug-on" class="btn-check-debug" name="fDebug" value="on" 
-    <?= $_SESSION['settings']['debug'] ? "checked" : "";?> onclick="submit()"/>
+    <input 
+      type="radio" 
+      id="btn-check-debug-on" 
+      class="btn-check-debug" 
+      name="fDebug" 
+      value="on" 
+      onclick="submit()"
+      <?= $_SESSION['settings']['debug'] ? "checked" : "";?>
+    />
+
     <label for="btn-check-debug-off" class="btn-ctr">Off: </label>
-    <input type="radio" id="btn-check-debug-off" class="btn-check-debug" name="fDebug" value="off" 
-    <?= $_SESSION['settings']['debug'] ? "" : "checked";?> onclick="submit();"/>
+    <input 
+      type="radio" 
+      id="btn-check-debug-off" 
+      class="btn-check-debug" 
+      name="fDebug" value="off" 
+      onclick="submit();"
+      <?= $_SESSION['settings']['debug'] ? "" : "checked";?> 
+    />
 </form>
 
 <?php
